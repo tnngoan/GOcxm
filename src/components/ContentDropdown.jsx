a6450776f58f28a4127a0f2980524ddbf2cfcfc3
tnngoan <ngoan.n.tr@gmail.com>
2023-02-13 14:53:35 +0700

    fix open 1 item per click

--- src/components/ContentDropdown.jsx
+++ src/components/ContentDropdown.jsx
@@ -10,14 +10,12 @@ const ContentDropdown = (props) => {
     return (
         <div>
             {
-                props.list.map((item) => (
-                    <div>
-                        <button className="flex" onClick={handleClick}>{item.title}
-                            {isOpen ? <div className="rotate-90 px-4"><i className="fa-solid fa-chevron-right" /></div> : <div className="px-4"><i className="fa-solid fa-chevron-right" /></div>}
-                        </button>
-                        <p className={isOpen ? 'flex' : 'hidden'}>{item.content}</p>
-                    </div>
-                ))
+                <div>
+                    <button className="flex" onClick={handleClick}>{props.item.title}
+                        {isOpen ? <div className="rotate-90 px-4"><i className="fa-solid fa-chevron-right" /></div> : <div className="px-4"><i className="fa-solid fa-chevron-right" /></div>}
+                    </button>
+                    <p className={isOpen ? 'flex' : 'hidden'}>{props.item.content}</p>
+                </div>
             }
         </div>
     )
