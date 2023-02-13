a8029c76d566c29151e2e78edf2a5c473772b294
tnngoan <ngoan.n.tr@gmail.com>
2023-02-13 14:45:21 +0700

    remove build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
