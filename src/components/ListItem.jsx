5ab516d13ea66a722b0d17bde710e41d5603f71e
Nhu Ngoan Tran <ngoan.n.tr@gmail.com>
2023-02-19 20:31:48 +0700

    Update main 3 (#8)
    * edit spacing and fonts
    
    * update spacing

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,19 +1,19 @@
 const ListItem = (props) => {
-return (
-    <div>
-        {
-            <div className="text-white flex items-center justify-end w-3/5">
-                <div className="flex-cols items-center justify-end w-full text-right px-4">
-                    <h3>{props.item.title}</h3>
-                    <p>{props.item.content}</p>
+    return (
+        <div>
+            {
+                <div className="text-white flex items-start justify-end py-10 w-3/5">
+                    <div className="flex-cols items-center justify-end w-full text-right px-4 w-2/5">
+                        <h3 className="text-3xl text-orange-400">{props.item.title}</h3>
+                        <p className="text-2xl px-1">{props.item.content}</p>
+                    </div>
+                    <div>
+                        <img width='72px' src={props.item.src} />
+                    </div>
                 </div>
-                <div> 
-                <img width='48px' height='48px' src={props.item.src}/>
-                </div>
-            </div>
-        }
-    </div>
-)
+            }
+        </div>
+    )
 }
 
 export default ListItem;
